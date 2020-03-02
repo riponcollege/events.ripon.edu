b746071440b69accd97a73c5149ae371e8374770
James Pederson <james@jpederson.com>
2020-03-02 15:20:32 -0600

    Add additional button styles.

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 <?php wp_head(); ?>
 
 <?php if ( is_search() ) { ?><meta name="robots" content="noindex" /><?php } ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=8" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=9" rel="stylesheet" type="text/css">
 
 <!-- Facebook Pixel Code -->
 <script>
