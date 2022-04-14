1bcbdc9579f1204549ad768aa0a410af23f6c3bc
James Pederson <james@jpederson.com>
2022-04-14 14:44:25 -0500

    Add the google sites tag to the top of the body.

--- header.php
+++ header.php
@@ -41,23 +41,25 @@ src="https://www.facebook.com/tr?id=737054459766387&ev=PageView
 </noscript>
 <!-- End Facebook Pixel Code -->
 
-<!-- Google Tag Manager -->
-<script
->(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5LJ7X4B');</script>
-<!-- End Google Tag Manager -->
-
 <meta name="google-site-verification" content="doR_WBYOA2Pk9RUcQApNpunbwH9V27FzukKp2r3p4vU" /> 
 
 </head>
 <body <?php body_class(); ?>>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VCS5HW7JEF"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-VCS5HW7JEF');
+</script>
+
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LJ7X4B"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
 <header>
 
 	<div class="wrap">
